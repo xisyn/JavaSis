2b3524fcdc0fb4731dff82f20d18a3689d228bf8
xisyn <xisyndi@gmail.ru>
2019-08-21 21:06:44 +0700

    add unit5 template

---
+++ unit-5/src/main/java/PathFinder.java
@@ -0,0 +1,12 @@
+import entity.DeliveryTask;
+import entity.Transport;
+
+import java.util.List;
+
+public class PathFinder {
+
+    public Transport getOptimalTransport(DeliveryTask deliveryTask, List<Transport> transports) {
+        return null;
+    }
+
+}
