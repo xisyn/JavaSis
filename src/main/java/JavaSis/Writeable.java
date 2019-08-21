63abefaa77438786dfa18289115e32f8a5426100
xisyn <xisyndi@gmail.ru>
2019-08-21 20:21:27 +0700

    merge unit4 to the separate folder

--- src/main/java/JavaSis/Writeable.java
+++
@@ -1,9 +0,0 @@
-package JavaSis;
-
-import java.io.FileWriter;
-import java.util.List;
-
-public interface Writeable<T> {
-    T parseLine(String line);
-    void write(FileWriter writer, String stringSeparator, int index, List<T> list);
-}
