13ad98a646c1139015b857b85245463613bd2abb
xisyn <xisyndi@gmail.ru>
2019-08-26 19:34:20 +0700

    Fixed according to comments

--- unit-5/src/main/java/PathFinder.java
+++ unit-5/src/main/java/PathFinder.java
@@ -3,8 +3,12 @@ import entity.Route;
 import entity.RouteType;
 import entity.Transport;
 
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static java.util.Map.Entry.comparingByValue;
 import static java.util.stream.Collectors.toMap;
@@ -22,31 +26,24 @@ public class PathFinder {
      * Получить map транспорта с ценами, отсортированный по увеличению цены
      */
     private Map<Transport, Double> getSortedTransportsWithPrices(DeliveryTask deliveryTask, List<Transport> transports) {
-        List<Double> prices = transports
-                .stream()
-                .map(transport -> getTransportPrice(deliveryTask, transport))
-                .collect(Collectors.toList());
-
-        Map<Transport, Double> transportWithPrice = new HashMap<>();
-        for (int i=0; i < transports.size(); i++) {
-            transportWithPrice.put(transports.get(i), prices.get(i));
-        }
-
-        return transportWithPrice
+        return IntStream.range(0, transports.size()).boxed()
+                .collect(Collectors.toMap(transports::get, transports.stream()
+                        .map(transport -> getTransportPrice(deliveryTask, transport))
+                        .collect(Collectors.toList())::get))
                 .entrySet()
                 .stream()
                 .sorted(comparingByValue())
-                .collect(
-                        toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2,
-                                LinkedHashMap::new));
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2,
+                        LinkedHashMap::new));
     }
 
     /**
      * Отфильтровать транспорт по типу маршрута и объему груза из таски
      */
     private List<Transport> getFilteredTransports(DeliveryTask deliveryTask, List<Transport> transports) {
+        List<RouteType> routeTypes = getRouteTypes(deliveryTask);
         return transports.stream()
-                .filter(transport -> getRouteTypes(deliveryTask).contains(transport.getType()))
+                .filter(transport -> routeTypes.contains(transport.getType()))
                 .filter(transport -> transport.getVolume() >= deliveryTask.getVolume())
                 .collect(Collectors.toList());
     }
@@ -65,12 +62,11 @@ public class PathFinder {
      */
     private double getTransportPrice(DeliveryTask task, Transport transport) {
         double price = transport.getPrice();
-        double length = 0;
-        for (Route route : task.getRoutes()) {
-            if (route.getType().equals(transport.getType())) {
-                length = route.getLength();
-            }
-        }
+        double length = task.getRoutes().stream()
+                .filter(route -> route.getType().equals(transport.getType()))
+                .map(Route::getLength)
+                .findFirst().orElse((double) 0);
+
         return price * length;
     }
 }
